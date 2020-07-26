b4d6cdfdd1b64bbbb2545d48bc0789f67f67c197
ckofor <oforchinedukelechi@gmail.com>
2020-07-26 18:03:02 +0100

    new changes

---
+++ src/Signup/Signup.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './Signup.css'
+
+const Signup = () => {
+  
+  return (
+    <div className='Signup-page'>
+      <h1
+        style={{
+          margin: 0
+        }}
+      >
+        hello from signup
+      </h1>
+    </div>
+  );
+}
+
+export default Signup;
---
+++ src/landing/Landing.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Landing.css'
+
+const Landing = () => {
+  
+  return (
+    <div className='landing-page'>
+      <h1>hello from landing</h1>
+    </div>
+  );
+}
+
+export default Landing;
